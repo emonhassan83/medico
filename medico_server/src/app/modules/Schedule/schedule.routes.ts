e3b3618f56102e1120ab2098baff8ea04df69f5a
Emon <emonhasan7650@gmail.com>
2024-11-25 14:10:12 +0600

    schedule validation added done

--- medico_server/src/app/modules/Schedule/schedule.routes.ts
+++ medico_server/src/app/modules/Schedule/schedule.routes.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { ScheduleController } from "./schedule.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import validateRequest from "../../middlewares/validateRequest";
+import { ScheduleValidation } from "./schedule.validation";
 
 const router = express.Router();
 
 router.post(
   "/",
   auth(UserRole.RECEPTIONIST, UserRole.ADMIN),
+  validateRequest(ScheduleValidation.create),
   ScheduleController.insertIntoDB
 );
 
