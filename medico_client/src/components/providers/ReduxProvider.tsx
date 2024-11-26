fbd95458ac5cb4f307e1c1107d85476ba8fb527c
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2024-11-26 23:19:01 +0600

    add redux setup in this project

---
+++ medico_client/src/components/providers/ReduxProvider.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { store } from "@/redux/store"
+import { Provider } from "react-redux"
+
+
+
+const ReduxProvider = ({children}:{ children:React.ReactNode }) => {
+  return (
+    <div>
+        <Provider store={store}> 
+            {children}
+        </Provider>
+    </div>
+  )
+}
+
+export default ReduxProvider;
