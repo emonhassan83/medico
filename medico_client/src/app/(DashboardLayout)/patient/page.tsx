76e97ef1b9dad191e5181451d7e59a3aef59440c
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2024-11-30 17:17:45 +0600

    added rout based patient data

--- medico_client/src/app/(DashboardLayout)/patient/page.tsx
+++ medico_client/src/app/(DashboardLayout)/patient/page.tsx
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 
-const Patient = async () => {
-  const res = await fetch(
-    "https://medico-server-gray.vercel.app/api/v1/patient"
-  ).then((data) => data.json());
-=======
-import React from 'react'
->>>>>>> 73d993fc0bd60bf5d90fabf6f9da75f216bc6cba
-
 const PatientDashboard = () => {
-  return (
-    <div>PatientDashboard</div>
-  )
-}
+  return <div>PatientDashboard</div>;
+};
 
-export default PatientDashboard
+export default PatientDashboard;
