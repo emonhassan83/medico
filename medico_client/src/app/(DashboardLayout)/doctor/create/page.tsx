26e11ca12f79a49183e0662f4050c1d661fe18bb
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2024-11-30 16:19:45 +0600

    sm

--- medico_client/src/app/(DashboardLayout)/doctor/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreateDoctor = () => {
-  return (
-    <div>Create Doctor</div>
-  )
-}
-
-export default CreateDoctor
--- medico_client/src/app/(DashboardLayout)/doctor/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Doctor = () => {
-  return (
-    <div>Doctor page</div>
-  )
-}
-
-export default Doctor
