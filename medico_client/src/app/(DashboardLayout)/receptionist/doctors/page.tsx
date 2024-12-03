e5da5d3d0dfff728e0deb52954ba8e106a121de4
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2024-12-04 00:55:15 +0600

    added all old data

--- medico_client/src/app/(DashboardLayout)/receptionist/doctors/page.tsx
+++ medico_client/src/app/(DashboardLayout)/receptionist/doctors/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import DoctorTableInReceptionist from "@/components/Dashboard/Receptionist/Doctor/DoctorTableInReceptionist";
+import Link from "next/link";
+import React from "react";
+import { BsSlash } from "react-icons/bs";
 
 const Doctor = () => {
   return (
-    <div>Doctor page</div>
-  )
-}
+    <div className="mx-5">
+      <div className="flex items-center justify-between mt-2">
+        <div>
+          <h2 className="text-lg text-[#495057] font-semibold">DOCTOR LIST</h2>
+        </div>
+        <div className="flex items-center gap-1 text-[#495057] text-sm">
+          <Link href="#" className="">
+            Dashboard
+          </Link>
+          <BsSlash className="text-[#ccc]" />
+          <Link href="#">Doctors</Link>
+        </div>
+      </div>
 
-export default Doctor
+      <div className="pt-5">
+        <DoctorTableInReceptionist />
+      </div>
+    </div>
+  );
+};
+
+export default Doctor;
