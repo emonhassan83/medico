cd4393cad781fec810afcd36b3533c0a813ff48c
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2024-12-07 17:32:07 +0600

    added appointment of receptionist

--- medico_client/src/app/(DashboardLayout)/receptionist/appointments/page.tsx
+++ medico_client/src/app/(DashboardLayout)/receptionist/appointments/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import CreateCalenderInDoctor from "@/components/Dashboard/Doctor/Appointment/CreateCalenderInDoctor";
+import CreateCalender from "@/components/Dashboard/PatientAppointment/CreateCalender";
+import CreateCalenderInReception from "@/components/Dashboard/Receptionist/Appointment/CreateCalenderInReception";
+import Link from "next/link";
+import React from "react";
+import { BsSlash } from "react-icons/bs";
 
-const page = () => {
+const AppointmentPageOfReceptionist = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="mx-5">
+      <div className="flex items-center justify-between mt-2">
+        <div>
+          <h2 className="text-lg text-[#495057] font-semibold">
+            Book Appointment
+          </h2>
+        </div>
+        <div className="flex items-center gap-1 text-[#495057] text-sm">
+          <Link href="#" className="">
+            Dashboard
+          </Link>
+          <BsSlash className="text-[#ccc]" />
+          <Link href="#">Appointment</Link>
+        </div>
+      </div>
+      <div className="mt-5">
+        <Link
+          href="/receptionist/appointments/create"
+          className="text-white text-sm bg-[#556ee6] py-2 px-4 rounded-md"
+        >
+          + New Appointment
+        </Link>
+      </div>
+      <div>
+        <CreateCalenderInReception />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default AppointmentPageOfReceptionist;
