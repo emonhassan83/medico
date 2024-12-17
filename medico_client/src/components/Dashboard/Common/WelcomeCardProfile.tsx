86856c8ce9ee6bff80b6b13e1e8f3d9d981b7e69
Emon <emonhasan7650@gmail.com>
2024-12-17 22:02:54 +0600

    edit profile page added done

--- medico_client/src/components/Dashboard/Common/WelcomeCardProfile.tsx
+++ medico_client/src/components/Dashboard/Common/WelcomeCardProfile.tsx
@@ -4,13 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { TiUserOutline } from "react-icons/ti";
 
-
-
 const WelcomeCardProfile = () => {
-  const {data} = useGetMyProfileQuery(undefined)
-
- 
- 
+  const { data } = useGetMyProfileQuery({});
 
   return (
     <div className="rounded">
@@ -23,7 +18,6 @@ const WelcomeCardProfile = () => {
         <div className="flex-1 ml-[30%] relative z-10 w-[100px] h-[70px]">
           <Image src={"/profile-img.png"} layout="fill" alt="" />
         </div>
-
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 p-5 bg-white font-light">
         <div className="col-span-1 flex flex-col gap-y-1 relative">
@@ -32,26 +26,25 @@ const WelcomeCardProfile = () => {
             icon={<TiUserOutline />}
             className="absolute -top-14 z-10 bg-gray-300"
           />
-         
         </div>
-
-
-        
       </div>
 
       <div className="flex justify-between px-5">
-           <div>
-           <p className="text-[#495057] text-md mb-2" >{`${data?.firstName} ${data?.lastName}...`}</p>
-           <p className="text-[#74788D]" >{data?.role}</p>
-           </div>
-        
-           <div>
-             <p className="text-[#495057] text-md mb-2" >Last login:</p>
-             <h1 className="text-[#74788D]">{data?.createdAt}</h1>
-             <button className="bg-blue-700 text-white px-4 py-1 rounded mt-4 ">Edit Profile</button>
-           </div>
-          
+        <div>
+          <p className="text-[#495057] text-md mb-2">{`${data?.firstName} ${data?.lastName}...`}</p>
+          <p className="text-[#74788D]">{data?.role}</p>
         </div>
+
+        <div>
+          <p className="text-[#495057] text-md mb-2">Last login:</p>
+          <h1 className="text-[#74788D]">{data?.createdAt}</h1>
+          <Link href="/patient/profile-view/edit">
+            <button className="bg-blue-700 text-white px-4 py-1 rounded mt-4 ">
+              Edit Profile
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
